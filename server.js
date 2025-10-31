86aaf08f63dfd8670dbc6f10377c1d074b462c46
josephine.kwao <kwaowiafejosephine@gmail.com>
2025-10-31 01:29:54 -0500

    modified test in the models folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -10,6 +10,7 @@ import questionRoutes from "./routes/questionRoutes.js";
 dotenv.config();
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -22,9 +23,9 @@ app.use("/api/questions", questionRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("✅ MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT}`)
+    console.log("✅ MongoDB connected");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
     );
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
