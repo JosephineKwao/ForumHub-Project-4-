a4ec2b4cc1a320a8f6465e24ea665969f3ba45a1
josephine.kwao <kwaowiafejosephine@gmail.com>
2025-10-31 02:48:36 -0500

    modified text in the server folder

--- server.js
+++ server.js
@@ -8,24 +8,25 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js";
 
 dotenv.config();
-const app = express();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/questions", questionRoutes);
 
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
-    );
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
