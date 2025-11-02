b6fa1b976f268013fc1c996bfa237fc2ba04592c
josephine.kwao <kwaowiafejosephine@gmail.com>
2025-11-02 00:25:50 -0500

    modified text in questionModel.js file and userModel .js file

--- server/models/questionModel.js
+++ server/models/questionModel.js
@@ -4,7 +4,13 @@ const questionSchema = new mongoose.Schema({
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   question: { type: String, required: true },
-  answer: { type: String },
+  answers: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      text: String,
+      createdAt: { type: Date, default: Date.now }
+    }
+  ],
   createdAt: { type: Date, default: Date.now },
 });
 
